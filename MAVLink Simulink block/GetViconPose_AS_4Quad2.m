a7935316ed819d1f5b81ed7c8392e7f0e4542a4e
wee kiat <acarde88@hotmail.com>
2014-08-19 13:37:10 +0800

    readjust four quad formation

--- MAVLink Simulink block/GetViconPose_AS_4Quad2.m
+++ MAVLink Simulink block/GetViconPose_AS_4Quad2.m
@@ -14,13 +14,13 @@ end
 
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('trial1', 'trial1');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('0801V1', '0801V1');
 ViconPose(1) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(2) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(3) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('trial1', 'trial1');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('0801V1', '0801V1');
 ViconPose(4) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(5) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(6) = QuadEulerXYZ.Rotation(3); % Yaw
@@ -32,13 +32,13 @@ ViconPose(6) = QuadEulerXYZ.Rotation(3); % Yaw
 
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('trial2', 'trial2');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('0802V1', '0802V1');
 ViconPose(7) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(8) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(9) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('trial2', 'trial2');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('0802V1', '0802V1');
 ViconPose(10) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(11) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(12) = QuadEulerXYZ.Rotation(3); % Yaw
@@ -49,13 +49,13 @@ ViconPose(12) = QuadEulerXYZ.Rotation(3); % Yaw
 
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('trial3', 'trial3');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('0803V1', '0803V1');
 ViconPose(13) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(14) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(15) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('trial3', 'trial3');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('0803V1', '0803V1');
 ViconPose(16) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(17) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(18) = QuadEulerXYZ.Rotation(3); % Yaw
@@ -66,13 +66,13 @@ ViconPose(18) = QuadEulerXYZ.Rotation(3); % Yaw
 % *********************************
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('trial4', 'trial4');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('0804V1', '0804V1');
 ViconPose(19) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(20) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(21) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('trial4', 'trial4');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('0804V1', '0804V1');
 ViconPose(22) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(23) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(24) = QuadEulerXYZ.Rotation(3); % Yaw
