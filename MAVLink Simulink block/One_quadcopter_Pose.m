52887a953e718e8e0eedcfa3bf4869c0dfc711c0
wee kiat <acarde88@hotmail.com>
2014-08-25 17:11:17 +0800

    recommit

--- MAVLink Simulink block/One_quadcopter_Pose.m
+++ MAVLink Simulink block/One_quadcopter_Pose.m
@@ -14,13 +14,13 @@ end
 
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('AS_330_1', 'AS_330_1');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('testa', 'testa');
 ViconPose(1) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(2) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(3) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('AS_330_1', 'AS_330_1');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('testa', 'testa');
 ViconPose(4) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(5) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(6) = QuadEulerXYZ.Rotation(3); % Yaw
