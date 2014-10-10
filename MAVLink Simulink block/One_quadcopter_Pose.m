6ec95f1b4c1e8ca5df20a76158220fe451450b8e
twk <wktan4@e.ntu.edu.sg>
2014-10-10 15:52:21 +0800

    Raw vicon object data collection file done

--- MAVLink Simulink block/One_quadcopter_Pose.m
+++ MAVLink Simulink block/One_quadcopter_Pose.m
@@ -14,13 +14,13 @@ end
 
 
 % X, Y, Z Global Coordinates of the Quad
-Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('0807V1', '0807V1');
+Output_GetSegmentGlobalTranslation = MyClient.GetSegmentGlobalTranslation('123', '123');
 ViconPose(1) = Output_GetSegmentGlobalTranslation.Translation(1);  % X-coordinate
 ViconPose(2) = Output_GetSegmentGlobalTranslation.Translation(2);  % Y-coordinate
 ViconPose(3) = Output_GetSegmentGlobalTranslation.Translation(3);  % Z-coordinate
 
 % Euler Angles
-QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('0807V1', '0807V1');
+QuadEulerXYZ = MyClient.GetSegmentGlobalRotationEulerXYZ('123', '123');
 ViconPose(4) = QuadEulerXYZ.Rotation(1); % Roll
 ViconPose(5) = QuadEulerXYZ.Rotation(2); % Pitch
 ViconPose(6) = QuadEulerXYZ.Rotation(3); % Yaw
